dc805b4cce767dd87f4245467e9881542049b8ec
Wati-Dev <raphaelgrougnetca@gmail.com>
2023-09-06 15:06:21 -0400

    Changement de la route add-ask

--- rers/urls.py
+++ rers/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 # Importe le fichier views depuis le dossier courant
 from . import views
 
@@ -9,8 +11,8 @@ urlpatterns = [
     path('accounts/register/', views.register, name='register'),
     path('add-offer/', views.add_offer, name='add_offer'),
     path('add-know/', views.add_know, name='add_know'),
-    path('add-ask/<int:id>', views.add_ask, name='add_ask'),
     path('details-offer/<int:id>', views.details_offer, name='details_offer'),
+    path('api/add-ask/<int:id>', views.add_ask, name='add_ask'),
     path('api/delete-offer/<int:id>', views.delete_offer, name='delete_offer'),
     path('api/modify-status-offer/<int:id>', views.modify_status_offer, name='modify_status_offer'),
     path('api/delete-ask/<int:id>', views.delete_ask, name='delete_ask'),
