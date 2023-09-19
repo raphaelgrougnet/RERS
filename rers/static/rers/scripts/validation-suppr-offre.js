6af4e86f39e140f70660f5afdb4222b2faa6cb44
Wati-Dev <raphaelgrougnetca@gmail.com>
2023-09-19 08:11:54 -0400

    suppr fonction test

--- rers/static/rers/scripts/validation-suppr-offre.js
+++ rers/static/rers/scripts/validation-suppr-offre.js
@@ -8,20 +8,3 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     })
 });
-
-(async () => {
-    const teasers = await fetch("https://api/gottinder.com/v2/fast-match/teasers", {
-        hearders: {
-            'X-Auth-Token' : localStorage.getItem('TinderWeb/APIToken')
-        }
-    }).then(res => res.json()).then(res => res.data.results);
-    const teaserEls = document
-        .querySelectorAll('.Expand.enterAnimationContainer > div:nth-child(1)');
-    for (let i = 0; i < teaserEls.length; i++) {
-        const teaser = teasers[i];
-        const teaserEl = teaserEls[i];
-        const teaserImage = teaser.user.photos[0].url;
-
-        teaserEl.style.backgroundImage = `url(${teaserImage})`;
-    }
-})();
